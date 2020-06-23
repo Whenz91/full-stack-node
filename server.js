6c8306067cf22c70eea3ce2ce745a327289c2785
Zoltan Miko <mikozoltan91@gmail.com>
2020-06-23 19:32:58 +0200

    initial task completed

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
+if(process.env.NODE_ENV !== 'production') {
+    require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require('body-parser');
 
 const indexRouter = require("./routes/index");
 
@@ -9,11 +14,13 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/testdb", {
-    useNewUrlParser: "true"
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: "true",
+    useUnifiedTopology: "true"
 });
 const db = mongoose.connection;
 db.on("error", error => console.error(error));
