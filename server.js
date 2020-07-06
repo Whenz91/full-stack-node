191ba9ddd636d452c583a7c2c8411987e018bd50
Zoltan Miko <mikozoltan91@gmail.com>
2020-07-06 20:16:35 +0200

    Initial Author Routs

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const expressLayouts = require("express-ejs-layouts");
 const bodyParser = require('body-parser');
 
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
@@ -27,5 +28,6 @@ db.on("error", error => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose"));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000);
